eed912841e61aa5a5d5d6b2c6a91758c7d722aa1
amosyung <amosyung@hotmail.com>
2013-12-03 23:30:57 -0500

    Check in #3

---
+++ Kobo.ContactManager.ServiceHost/ContactManagerFacade.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Kobo.ContactManager.Contract.Service;
+using Spring.Core;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace Kobo.ContactManager.ServiceHost
+{
+    public class ContactManagerFacade : IContactManagerService
+    {
+
+        public IEnumerable<PersonDTO> SearchForContacts(IEnumerable<string> searchParameter, int fromPosition, int recordsToReturn)
+        {
+            return ContactManager.SearchForContacts(searchParameter, fromPosition, recordsToReturn);
+        }
+
+        public PersonDTO GetContact(int contactId)
+        {
+            return ContactManager.GetContact(contactId);
+        }
+
+        public ContactUpdateResponse AddContact(PersonDTO contact)
+        {
+            return ContactManager.AddContact(contact);
+        }
+
+        public ContactUpdateResponse DeleteContact(int contactId)
+        {
+            return ContactManager.DeleteContact(contactId);
+        }
+
+        public ContactUpdateResponse UpdateContact(PersonDTO contact)
+        {
+            return ContactManager.UpdateContact(contact);
+        }
+
+        private IContactManagerService ContactManager
+        {
+            get
+            {
+                IApplicationContext context = ContextRegistry.GetContext();
+                IContactManagerService mgr = context.GetObject("ContactManager") as IContactManagerService;
+                return mgr;
+            }
+        }
+
+
+    }
+}
