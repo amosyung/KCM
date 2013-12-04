51a83c47a7997d2ed0fa8832c691ee3f8e9fdeee
amosyung <amosyung@hotmail.com>
2013-12-03 22:56:11 -0500

    Commit 1

---
+++ Kobo.ContactManager.Contract/Enum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kobo.ContactManager.Contract
+{
+    public enum SearchByEnums
+    {
+        ByAny,
+        ByFirstName,
+        ByLastName
+    }
+
+    public enum OrderByEnums
+    {
+        ById,
+        ByLastName,
+        ByFirstName,
+        ByDateOfBirth
+    }
+
+    public enum ContactValidationErrors
+    {
+        ContactNotFound,
+        ContactTypeNotSupported,
+        FirstNameRequired,
+        FirstNameLengthMismatch,
+        LastNameRequired,
+        LastNameLengthMismatch,
+        PhoneNumberRequired,
+        PhoneNumberInvalid,
+        InvalidEmail,
+        BirthDateInvalid,
+        BirthDateInTheFuture
+    }
+}
